859d7cfa97755a2bbe0c6730f7e1d134215c6433
Chris <chris.pieonte@hotmail.it>
2015-07-21 20:09:59 +0200

    Other changes

--- app/src/main/java/it/uniba/di/sss1415/medicalmentoring/Home.java
+++ app/src/main/java/it/uniba/di/sss1415/medicalmentoring/Home.java
@@ -1,26 +1,39 @@
 package it.uniba.di.sss1415.medicalmentoring;
 
 //import android.app.Fragment;
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.ActionBar;
 
 
 public class Home extends ActionBarActivity {
-    
+
+    CustomPagerAdapter mCustomPagerAdapter;
+    ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        if (savedInstanceState == null)
+        /*if (savedInstanceState == null)
         {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.homeFrame, new Appuntamenti()).commit();
 
-        }
+        }*/
+
+        mCustomPagerAdapter = new CustomPagerAdapter(getSupportFragmentManager(), this);
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mCustomPagerAdapter);
     }
     
     @Override
@@ -44,4 +57,54 @@ public class Home extends ActionBarActivity {
         
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
+
+    class CustomPagerAdapter extends FragmentPagerAdapter {
+
+        Context mContext;
+
+        public CustomPagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            mContext = context;
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+
+            if(pos == 0){
+                return new Appuntamenti();
+            }else{
+                return new Notifiche();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int pos) {
+            if(pos == 0){
+                return "APPUNTAMENTI";
+            }else{
+                return "NOTIFICHE";
+            }
+        }
+
+    }
+
+    /*<FrameLayout
+    android:id="@+id/homeFrame"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:layout_alignParentTop="true"
+    android:layout_alignParentLeft="true"
+    android:layout_alignParentStart="true">
+
+    </FrameLayout>*/
+
 }
