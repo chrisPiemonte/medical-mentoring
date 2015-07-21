7ada5dddd80f32db5abfd6557d6c5015bb31f74a
paolobinni <paolo.saracino.ps@gmail.com>
2015-07-21 22:26:22 +0200

    Search button

--- app/src/main/java/it/uniba/di/sss1415/medicalmentoring/Home.java
+++ app/src/main/java/it/uniba/di/sss1415/medicalmentoring/Home.java
@@ -2,6 +2,7 @@ package it.uniba.di.sss1415.medicalmentoring;
 
 //import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -35,12 +36,14 @@ public class Home extends ActionBarActivity {
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mCustomPagerAdapter);
     }
-    
+
+    // ------ da qui per le modifiche per i bottoni sulla barra
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
+
+        return super.onCreateOptionsMenu(menu);
     }
     
     @Override
@@ -48,17 +51,21 @@ public class Home extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_search:
+                mostraCalendar();
+                return true;
+            case R.id.action_settings:
+                return true;
         }
-        
+
         return super.onOptionsItemSelected(item);
     }
 
-
+    public  void mostraCalendar(){
+        Intent apri = new Intent(Home.this, Calendar.class);
+        startActivity(apri);
+    }
 
 
 
